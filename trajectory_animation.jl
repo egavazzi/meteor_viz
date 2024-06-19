d1eee769cf76c92fb85bd7d261686789fad75a31
Juha Vierinen <jvierine@gmail.com>
2024-06-19 12:15:11 +0000

    added more lines. fixed indexing. I dislike 1:n indexing of Julia.

--- trajectory_animation.jl
+++ trajectory_animation.jl
@@ -1,76 +1,102 @@
+
 using GLMakie
 using HDF5
 using ProgressMeter
 
+function plotfun()
 
-## Load the data
-hid = h5open("combined_results.h5", "r")
+    ## Load the data
+    hid = h5open("../maarsy_meteors/daniel/results/combined_results.h5", "r")
 
-# initialize arrays
-n_events = length(keys(hid["events"]))
-n_t = length(read(hid["events/0/orbit"])[1:end, 1])
-lines_data = fill(NaN, (n_events * (n_t + 1), 3))
-# load the data and concatenate them to create only one line object
-@showprogress for (i, i_event) in enumerate(keys(hid["events"]))
-    string_dataset = "events/" * i_event * "/orbit"
-    data = read(hid[string_dataset])
-    lines_data[(1:n_t) .+ 1 .+ (i - 1) * (n_t + 1), :] .= data[1:end, 1:3]
-end
+    vel = read(hid["vels"])
+    vel[vel .> 70000] .= 70000
+    vel[vel .< 10000] .= 10000
 
-close(hid)
-
-
-
-## Plot the data (animate one meteor)
-points1 = Observable(Point3f[])
-points2 = Observable(Point3f[])
-points3 = Observable(Point3f[])
-colors = Observable(Int[])
-
-set_theme!(theme_black())
-f = Figure()
-ax = Axis3(f[1, 1])
-l1 = lines!(points1, color = colors, colormap = :inferno, transparency = true)
-l2 = lines!(points2, color = colors, colormap = :inferno, transparency = true)
-l3 = lines!(points3, color = colors, colormap = :inferno, transparency = true)
-xlims!(-10 * 150e9, 10 * 150e9)
-ylims!(-10 * 150e9, 10 * 150e9)
-zlims!(-10 * 150e9, 10 * 150e9)
-display(f)
-set_theme!()
-
-# animate
-while true
-    isopen(f.scene) || break # exit if window is closed
-    for i_t in 1:n_t
-        isopen(f.scene) || break  # exit if window is closed
-
-        push!(points1[], lines_data[i_t + 1, :])
-        push!(points2[], lines_data[2 * (n_t + 1) + i_t + 1, :])
-        push!(points3[], lines_data[4 * (n_t + 1) + i_t + 1, :])
-        push!(colors[], i_t)
-        notify(points1)
-        notify(points2)
-        notify(points3)
-        notify(colors)
-        l1.colorrange = (0, length(points1[]))
-        l2.colorrange = (0, length(points2[]))
-        l3.colorrange = (0, length(points3[]))
-        if length(points1[]) > 1000
-            popfirst!(points1[])
-            popfirst!(points2[])
-            popfirst!(points3[])
-        end
-
-        sleep(0.001)
+    # this is so messed up
+    # the event key is index 0..n-1 to
+    # arrays like vel
+    event_keys=[]
+    eks=keys(hid["events"])
+    for i in 1:length(eks)
+        push!(event_keys,parse(Int,eks[i]))
+    end
+    
+    # initialize arrays
+    n_events = length(keys(hid["events"]))
+    n_t = length(read(hid["events/0/orbit"])[1:end, 1])
+    lines_data = fill(NaN, (n_events * (n_t + 1), 3))
+    # load the data and concatenate them to create only one line object
+    @showprogress for (i, i_event) in enumerate(keys(hid["events"]))
+        string_dataset = "events/" * i_event * "/orbit"
+        data = read(hid[string_dataset])
+        
+        lines_data[n_t*i .+ (1:n_t), :] .= data[1:end, 1:3]
     end
 
-    points1.val = Point3f[]
-    points2.val = Point3f[]
-    points3.val = Point3f[]
-    colors.val = Int[]
-    notify(points1)
-    notify(points2)
-    notify(points3)
-    notify(colors)
+    close(hid)
+    
+    set_theme!(theme_black())
+    f = Figure()
+    ax = Axis3(f[1, 1])
+    max_au=5
+    xlims!(-max_au * 150e9, max_au * 150e9)
+    ylims!(-max_au * 150e9, max_au * 150e9)
+    zlims!(-max_au * 150e9, max_au * 150e9)
+    display(f)
+
+
+    pointlist=[]
+    linelist=[]
+    evidx=[]
+    n_lines=1000
+    colors = Observable(Int[])
+    n_colors=10
+    for i = 1:n_lines
+        o=Observable(Point3f[])
+        push!(pointlist,o)
+        idx=Int(round(rand()*n_events+1))
+        push!(evidx,idx)
+#        l=lines!(o, color = colors, colormap = :grays, transparency = true, alpha=0.2)
+        l=lines!(o, color = vel[event_keys[idx]+1], colorrange=(10e3,70e3), colormap=:turbo, transparency = true, alpha=0.2)        
+#        l.colorrange = (0, n_colors)#ceil(n_t/10))
+        push!(linelist,l)
+    end
+    
+    
+    # animate
+    while true
+
+        isopen(f.scene) || break # exit if window is closed
+ #       color_idx=0
+        for i_t in 1:10:n_t
+            isopen(f.scene) || break  # exit if window is closed
+#            push!(colors[], color_idx)            
+            for i = 1:n_lines
+                push!(pointlist[i][], lines_data[evidx[i]*n_t + i_t, :])
+                notify(pointlist[i]  )
+                if length(pointlist[i][]) > n_colors
+                    popfirst!(pointlist[i][])
+                end
+            end
+  #          color_idx+=1
+#            notify(colors)
+            sleep(0.0000001)
+        end
+        # randomize events to plot
+        for i = 1:n_lines
+            evidx[i]=Int(round(rand()*n_events+1))
+            linelist[i]= lines!(o, color = vel[evidx[i]], colorrange=(10e3,70e3), colormap=:turbo, transparency = true, alpha=0.2)        
+        end
+        for i = 1:n_lines
+            pointlist[i].val=Point3f[]
+            
+            notify(pointlist[i])
+        end
+#        colors.val=Int[]
+ #       notify(colors)
+    end
 end
+
+
+
+plotfun()
